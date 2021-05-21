d0ed504d786ded9e38c869cccc44d1603da1a333
jankubatt <jankubat.1@seznam.cz>
2021-05-21 22:22:03 +0200

    blog and major changes

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+let clicked = sessionStorage.getItem("clicked");
+
+$(document).ready(function () {
+	$("a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1200, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+});
+
+$(".dropdown").on("click", function() {
+	sessionStorage.setItem("clicked", "true");
+});
+
+$.getJSON("https://geo.ipify.org/api/v1?apiKey=at_5hNQ4Gjv5vH77KEL0pwd2eT3y3aNI", function(data) {
+	if (data.location.country == "CZ" && clicked == null) {
+		window.location.replace("./cz");
+	}
+});
